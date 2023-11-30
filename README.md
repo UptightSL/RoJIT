50c72091cbadb6eb283485e3a5b4fbf71bfe2644
Uptight <72242372+UptightSL@users.noreply.github.com>
2023-11-30 15:05:30 +0000

    Creation of RoJIT + r_type vars

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RoJIT
-Roblox API Clone for LuaJIT
+
+Custom implementation of the Roblox Lua API for LuaJIT. This is not a 1-1 recreation, and never will be. snake_case is used for all code in here, please be wary of that.
---
+++ rojit.lua
@@ -0,0 +1,17 @@
+--[[
+    RoJIT - Made for Luvit/LuaJIT - Replicates various parts of the Roblox API, All in one file.
+
+    This is meant to be used for *custom* specific scripts. Mostly for POC's and whatnot.
+    It will make sure your code runs, so when you translate it to Luau everything goes smooth-sailing.
+]]
+
+--> * - r_type and r_type_mt are used internally, hence why it's not a global variable
+--> * - r_type is the main variable used for setting up types and whatnot. .
+local r_type, r_type_mt do
+    r_type = newproxy(true)
+    r_type_mt = getmetatable(r_type)
+    
+    r_type_mt.__tostring = function()
+        return ("ROJIT-R-TYPE-SYSTEM") 
+    end
+end
