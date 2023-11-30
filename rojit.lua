d12002b09cf65bc97db52681a8c44982bca7e728
Uptight <72242372+UptightSL@users.noreply.github.com>
2023-11-30 18:30:03 +0000

    Typos + Added `r_socket`, `typeof` and `wait`

--- rojit.lua
+++ rojit.lua
@@ -1,9 +1,23 @@
 --[[
-    RoJIT - Made for Luvit/LuaJIT - Replicates various parts of the Roblox API, All in one file.
+    RoJIT - Made for LuaJIT - Replicates various parts of the Roblox API, All in one file.
 
     This is meant to be used for *custom* specific scripts. Mostly for POC's and whatnot.
     It will make sure your code runs, so when you translate it to Luau everything goes smooth-sailing.
+
+    Notice: This is a rewritten implementation of Lemur. Code is modified and obviously written in just one file.
 ]]
+
+--> r_socket is the socket library for luajit
+local r_socket do
+    local success, socket = pcall(require, "socket")
+
+    if (not success) then
+        print("\"socket\" library is required for the following functions:\t\"wait()\"")
+        return
+    end
+
+    r_socket = socket
+end
 
 --> r_type and r_type_mt are used internally, hence why it's not a global variable
 --> r_type is the main variable used for setting up types and whatnot.
@@ -14,5 +28,35 @@ local r_type, r_type_mt do
     
     r_type_mt.__tostring = function()
         return ("ROJIT-R-TYPE-SYSTEM") 
+    end
+end
+
+--> typeof - custom function in Roblox, used for advanced types.
+--> wait - custom function in Roblox, used for setting up "sleeps"/waits.
+do
+    function _G.typeof(object)
+        local object_type = type(object)
+
+        if (object_type == "userdata") then
+            local object_metatable = getmetatable(object)
+
+            if (object_metatable == nil) then
+                return ("userdata")
+            end
+
+            if (object_metatable[r_type] == nil) then
+                return ("userdata")
+            end
+
+            return object_metatable[r_type]
+        end
+
+        return object_type
+    end
+
+    function _G.wait(delay)
+        local delay = delay * 1000
+        
+        r_socket.sleep(delay)
     end
 end
