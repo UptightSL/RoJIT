1ffcc4e3a604c17429032c8247c4bb887fc50113
Uptight <72242372+UptightSL@users.noreply.github.com>
2023-11-30 15:06:54 +0000

    Comment cleanup for r_type

--- rojit.lua
+++ rojit.lua
@@ -5,8 +5,9 @@
     It will make sure your code runs, so when you translate it to Luau everything goes smooth-sailing.
 ]]
 
---> * - r_type and r_type_mt are used internally, hence why it's not a global variable
---> * - r_type is the main variable used for setting up types and whatnot. .
+--> r_type and r_type_mt are used internally, hence why it's not a global variable
+--> r_type is the main variable used for setting up types and whatnot.
+--> r_type_mt is a direct reference to r_type's metatable
 local r_type, r_type_mt do
     r_type = newproxy(true)
     r_type_mt = getmetatable(r_type)
