3b7b905bcbe518edb99500f54f17f8f84f81d834
Uptight <72242372+UptightSL@users.noreply.github.com>
2023-12-01 12:59:16 +0000

    Reimplemented Enums

--- rojit.lua
+++ rojit.lua
@@ -20,6 +20,7 @@ local r_type, r_type_mt do
 end
 
 --> r_typeof - custom function in Roblox, used for advanced types.
+--> r_wait - custom function in Roblox, used for setting up "sleeps"/waits.
 local r_typeof do
     function r_typeof(object)
         local object_type = type(object)
@@ -41,3 +42,131 @@ local r_typeof do
         return object_type
     end
 end
+
+--[[
+    r_enum_item & r_enum rewritten from: https://github.com/raphtalia/Enum/
+]]
+
+local r_enum_item do
+    r_enum_item = {}
+
+    function r_enum_item.new(item_name, item_value, item_type)
+        local item_type = tostring(item_type)
+
+        local props do
+            props = {
+                name = item_name,
+                value = item_type,
+                enum_type = item_type
+            }
+        end
+
+        local enum_item_proxy do
+            enum_item_proxy = newproxy(true)
+
+            do
+                local enum_item_proxy_mt = getmetatable(enum_item_proxy)
+
+                enum_item_proxy_mt.__metatable = "the metatable is locked"
+
+                enum_item_proxy_mt.__tostring = function()
+                    return ("Enum.%s.%s"):format(item_type, item_name)
+                end
+
+                enum_item_proxy_mt.__index = function(_, index)
+                    local value = props[index]
+
+                    if (not value) then
+                        --> Error has not yet been implemented...
+                        return
+                    end
+
+                    return value
+                end
+
+                enum_item_proxy_mt.__newindex = function(_, index)
+                    --> Error has not yet been implemented...
+                    return
+                end
+            end
+        end
+
+        return enum_item_proxy
+    end
+end
+
+local r_enum do
+    r_enum = {}
+
+    function r_enum.new(enum_name, enum_item_list)
+        local enum_items = {}
+
+        local enum_methods do
+            enum_methods = {}
+
+            function enum_methods:get_enum_items()
+                local list = {}
+
+                for _, enum_item in pairs(enum_items) do
+                    table.insert(list, enum_item)
+                end
+
+                return list
+            end
+        end
+
+        local enum_proxy do
+            enum_proxy = newproxy(true)
+
+            local enum_proxy_mt do
+                enum_proxy_mt = getmetatable(enum_proxy)
+
+                enum_proxy_mt.__metatable = "the metatable is locked"
+
+                enum_proxy_mt.__tostring = function()
+                    return enum_name
+                end
+
+                enum_proxy_mt.__index = function(_, index)
+                    local value = enum_methods[index] or enum_items[index]
+
+                    if (not value) then
+                        --> Error has not yet been implemented...
+                        return
+                    end
+
+                    return value
+                end
+
+                enum_proxy_mt.__newindex = function(_, index)
+                    --> Error has not yet been implemented...
+                    return
+                end
+
+                for index, value in pairs(enum_item_list) do
+                    local enum_item_name, enum_item_value;
+                    
+                    if (type(index) == "number") then
+                        enum_item_name = value
+                        enum_item_value = index
+
+                        if (not type(value) == "string") then
+                            --> Error has not yet been implemented...
+                            return
+                        end
+                    elseif (type(index) == "string") then
+                        enum_item_name = index
+                        enum_item_value = value
+                    else
+                        --> Error has not yet been implemented...
+                    end
+
+                    enum_items[enum_item_name] = r_enum_item.new(enum_item_name, enum_item_value, enum_proxy)
+                end
+            end
+        end
+
+        return enum_proxy
+    end
+end
+
